d523a471fe38aed4df7e877f9cbf88c494ab403b
Heri Andria Herdiana <andri@niagahoster.co.id>
2018-12-27 15:45:59 +0700

    add card count to dashboard

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\ApiLog;
 use App\Classes\Query\GetTransactionStatistic;
+use App\User;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
@@ -24,7 +26,20 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('dashboard.home');
+        $countOfUser = User::ofUser()->count();
+        $countOfAdmin = User::ofAdmin()->count();
+        $today = date('Y-m-d') . ' 00:00:00';
+        $countOfApiCallToday = ApiLog::where('created_at', '>=', $today)->count();
+        $countOfApiCallAll = ApiLog::count();
+
+        $params = [
+            'count_of_user' => $countOfUser,
+            'count_of_admin' => $countOfAdmin,
+            'count_of_api_call_today' => $countOfApiCallToday,
+            'count_of_api_call_all' => $countOfApiCallAll,
+        ];
+
+        return view('dashboard.home', $params);
     }
 
     public function test()
--- resources/views/dashboard/home.blade.php
+++ resources/views/dashboard/home.blade.php
@@ -32,11 +32,11 @@
                 <!-- START card-->
                 <div class="card flex-row align-items-center align-items-stretch border-0">
                     <div class="col-4 d-flex align-items-center bg-primary-dark justify-content-center rounded-left">
-                        <em class="icon-cloud-upload fa-3x"></em>
+                        <em class="icon-people fa-3x"></em>
                     </div>
                     <div class="col-8 py-3 bg-primary rounded-right">
-                        <div class="h2 mt-0">1700</div>
-                        <div class="text-uppercase">Uploads</div>
+                        <div class="h2 mt-0">{{ $count_of_user }}</div>
+                        <div class="text-uppercase">Users</div>
                     </div>
                 </div>
             </div>
@@ -44,13 +44,11 @@
                 <!-- START card-->
                 <div class="card flex-row align-items-center align-items-stretch border-0">
                     <div class="col-4 d-flex align-items-center bg-purple-dark justify-content-center rounded-left">
-                        <em class="icon-globe fa-3x"></em>
+                        <em class="icon-people fa-3x"></em>
                     </div>
                     <div class="col-8 py-3 bg-purple rounded-right">
-                        <div class="h2 mt-0">700
-                            <small>GB</small>
-                        </div>
-                        <div class="text-uppercase">Quota</div>
+                        <div class="h2 mt-0">{{ $count_of_admin }}</div>
+                        <div class="text-uppercase">Administrator</div>
                     </div>
                 </div>
             </div>
@@ -58,33 +56,25 @@
                 <!-- START card-->
                 <div class="card flex-row align-items-center align-items-stretch border-0">
                     <div class="col-4 d-flex align-items-center bg-green-dark justify-content-center rounded-left">
-                        <em class="icon-bubbles fa-3x"></em>
+                        <em class="icon-graph fa-3x"></em>
                     </div>
                     <div class="col-8 py-3 bg-green rounded-right">
-                        <div class="h2 mt-0">500</div>
-                        <div class="text-uppercase">Reviews</div>
+                        <div class="h2 mt-0">{{ $count_of_api_call_today }}</div>
+                        <div class="text-uppercase">Api Call Today</div>
                     </div>
                 </div>
             </div>
             <div class="col-xl-3 col-lg-6 col-md-12">
-                <!-- START date widget-->
+                <!-- START card-->
                 <div class="card flex-row align-items-center align-items-stretch border-0">
-                    <div class="col-4 d-flex align-items-center bg-green justify-content-center rounded-left">
-                        <div class="text-center">
-                            <!-- See formats: https://docs.angularjs.org/api/ng/filter/date-->
-                            <div class="text-sm" data-now="" data-format="MMMM"></div>
-                            <br>
-                            <div class="h2 mt-0" data-now="" data-format="D"></div>
-                        </div>
+                    <div class="col-4 d-flex align-items-center bg-green-dark justify-content-center rounded-left">
+                        <em class="icon-graph fa-3x"></em>
                     </div>
-                    <div class="col-8 py-3 rounded-right">
-                        <div class="text-uppercase" data-now="" data-format="dddd"></div>
-                        <br>
-                        <div class="h2 mt-0" data-now="" data-format="h:mm"></div>
-                        <div class="text-muted text-sm" data-now="" data-format="a"></div>
+                    <div class="col-8 py-3 bg-green rounded-right">
+                        <div class="h2 mt-0">{{ $count_of_api_call_all }}</div>
+                        <div class="text-uppercase">Api Call Of All Time</div>
                     </div>
                 </div>
-                <!-- END date widget-->
             </div>
         </div>
         <!-- END cards box-->
